2f89aa5a26479c49084c278de5cb24ea2d4dcfa9
Niall O'Kane <okanenm@gmail.com>
2015-08-24 15:48:51 -0700

    perform adaptive thesholding

--- img_processing/image_thresholding.py
+++ img_processing/image_thresholding.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as numpy
 from matplotlib import pyplot as plt
 
+# simple thresholding
+
 img = cv2.imread('../assets/gradient.png',0)
 ret,thresh1 = cv2.threshold(img,63,255,cv2.THRESH_BINARY)
 ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
@@ -19,3 +21,21 @@ for i in xrange(6):
 
 plt.show()
 
+# adaptive thresholding
+
+img = cv2.imread('../assets/sign.jpg', 0)
+img = cv2.medianBlur(img,5)
+
+ret,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+  cv2.THRESH_BINARY,11,2)
+th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+  cv2.THRESH_BINARY,11,2)
+titles = ['Original Image', 'Global Thresholding (v = 127)', 'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+images = [img, th1, th2, th3]
+
+for i in xrange(4):
+    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
