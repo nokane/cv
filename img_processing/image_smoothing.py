f938ed7b1250d95ec2d5e8a2b3c579a5b88fcf7f
Niall O'Kane <okanenm@gmail.com>
2015-08-24 16:58:22 -0700

    implement image blurring and gaussian blurring

--- img_processing/image_smoothing.py
+++ img_processing/image_smoothing.py
@@ -9,8 +9,20 @@ img = cv2.imread('../assets/rabbit.png')
 kernel = np.ones((5,5),np.float32)/25
 print kernel
 dst = cv2.filter2D(img,-1,kernel)
-plt.subplot(121),plt.imshow(img, 'gray'),plt.title('Original')
+
+# Image Blurring/Smoothing
+blur = cv2.blur(img,(5,5))
+
+# Gaussian Blur
+
+gaussBlur = cv2.GaussianBlur(img,(5,5),0)
+
+plt.subplot(1,4,1),plt.imshow(img),plt.title('Original')
 plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(dst),plt.title('Averaging')
+plt.subplot(1,4,2),plt.imshow(dst),plt.title('Averaging')
+plt.xticks([]), plt.yticks([])
+plt.subplot(1,4,3),plt.imshow(blur),plt.title('Blurred')
+plt.xticks([]), plt.yticks([])
+plt.subplot(1,4,4),plt.imshow(gaussBlur),plt.title('Gaussian Blur')
 plt.xticks([]), plt.yticks([])
 plt.show()
