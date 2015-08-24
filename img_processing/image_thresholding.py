e375dd7ca1bde56b45222d5f97bfb0962fc07853
Niall O'Kane <okanenm@gmail.com>
2015-08-24 16:23:21 -0700

    implement Otsu's Binarization

--- img_processing/image_thresholding.py
+++ img_processing/image_thresholding.py
@@ -23,7 +23,7 @@ plt.show()
 
 # adaptive thresholding
 
-img = cv2.imread('../assets/sign.jpg', 0)
+img = cv2.imread('../assets/cathedral.jpeg', 0)
 img = cv2.medianBlur(img,5)
 
 ret,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
@@ -38,4 +38,28 @@ for i in xrange(4):
     plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
     plt.title(titles[i])
     plt.xticks([]),plt.yticks([])
+plt.show()
+
+img = cv2.imread('../assets/greenbldg.jpg',0)
+# global thresholding
+ret1,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+# Otsu's thresholding
+ret2,th2 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+# Otsu's thresholding after Gaussian filtering
+blur = cv2.GaussianBlur(img,(5,5),0)
+ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+# plot all the images and their histograms
+images = [img, 0, th1,
+  img, 0, th2,
+  blur, 0, th3]
+titles = ['Original Noisy Image','Histogram','Global Thresholding (v=127)',
+  'Original Noisy Image','Histogram',"Otsu's Thresholding",
+  'Gaussian filtered Image','Histogram',"Otsu's Thresholding"]
+for i in xrange(3):
+    plt.subplot(3,3,i*3+1),plt.imshow(images[i*3],'gray')
+    plt.title(titles[i*3]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3,3,i*3+2),plt.hist(images[i*3].ravel(),256)
+    plt.title(titles[i*3+1]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3,3,i*3+3),plt.imshow(images[i*3+2],'gray')
+    plt.title(titles[i*3+2]), plt.xticks([]), plt.yticks([])
 plt.show()
