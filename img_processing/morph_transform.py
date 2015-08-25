db5a2db7eb75dba7deb676959fc483dbdb3e0d37
Niall O'Kane <okanenm@gmail.com>
2015-08-24 18:22:27 -0700

    Use dilation/opening/closing

--- img_processing/morph_transform.py
+++ img_processing/morph_transform.py
@@ -12,3 +12,20 @@ cv2.imshow('img',erosion)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+#dilation
+dilation = cv2.dilate(img, kernel, iterations = 1)
+cv2.imshow('img', dilation)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Opening - erosion followed by dilation
+opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+cv2.imshow('img', opening)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Closing - dilation followed by erosion
+closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+cv2.imshow('img', closing)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
