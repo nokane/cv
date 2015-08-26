ccc8b8def58e4e67b060c9548c8cc7716b939f54
Niall O'Kane <okanenm@gmail.com>
2015-08-26 15:50:37 -0700

    Limit which contours are displayed

--- img_processing/contours_app.py
+++ img_processing/contours_app.py
@@ -5,8 +5,22 @@ img = cv2.imread('../assets/greenbldg.jpg')
 imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 ret,thresh = cv2.threshold(imgray,127,255,0)
 _, contours, _ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-im = cv2.drawContours(img, contours, -1, (0,255,0), 3)
+
+# identify largest contour area
+cnts = []
+for index, cnt in enumerate(contours):
+    currentArea = cv2.contourArea(cnt)
+    if currentArea > 1000:
+        cnts.append(cnt)
+
+im = cv2.drawContours(img, cnts, -1, (0,255,0), 3)
 
 cv2.imshow('image', im)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# identify image moment
+
+cnt = contours[0]
+M = cv2.moments(cnt)
+print M
